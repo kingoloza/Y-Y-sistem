87a159d98022193ef0b46fb4b67fcf582a95ce3f
kingoloza <kingo2764558@gmail.com>
2026-05-25 01:17:43 -0500

    login funcional con alerta

--- login.js
+++ login.js
@@ -1,11 +1,11 @@
 console.log("login.js conectado correctamente")
 const boton = document.querySelector("button")
 boton.addEventListener("click", function() {
-const usuario = document.querySelector("input[type='text']").value
-const password = document.querySelector("input[type='password']").value
-if (usuario === "admin" && password === "1234"){
-    console.log("Bienvenido administrador");
-} else {
-    console.log("usuario o contraseña incorrectos");
-}
+    const usuario = document.querySelector("input[type='text']").value
+    const password = document.querySelector("input[type='password']").value
+    if (usuario === "admin" && password === "1234"){
+    alert("Bienvenido administrador");
+    } else {
+    alert("usuario o contraseña incorrectos");
+    }
 }); 
