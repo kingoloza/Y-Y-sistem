fb53c12bec04134f0ddf6fae8f9189575998be08
kingoloza <kingo2764558@gmail.com>
2026-06-04 21:04:41 -0500

    the last

---
+++ dashboard.js
@@ -0,0 +1,10 @@
+const links = document.querySelectorAll("nav a")
+const contenido = document.querySelector(".contenido")
+
+links.forEach(function(link) {
+    links.addEventListener("click", function(e) {
+        e.preventDefault();
+        const seccion = link.textContent;
+        contenido.innerHTML = "<h1>" + seccion + "</h1><p>Sección en construcción...</p>";
+    });
+});
