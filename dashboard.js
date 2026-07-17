0fad06a50fc4919aa57dc3b05f3d289b06d06519
kingoloza <kingo2764558@gmail.com>
2026-07-17 16:01:43 -0500

    dashboard.js funcional

--- dashboard.js
+++ dashboard.js
@@ -2,7 +2,7 @@ const links = document.querySelectorAll("nav a")
 const contenido = document.querySelector(".contenido")
 
 links.forEach(function(link) {
-    links.addEventListener("click", function(e) {
+    link.addEventListener("click", function(e) {
         e.preventDefault();
         const seccion = link.textContent;
         contenido.innerHTML = "<h1>" + seccion + "</h1><p>Sección en construcción...</p>";
